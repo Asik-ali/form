6321ac730966e231f8ff8610866a9b827d291c5b
Asik-ali <asik14923@gmail.com>
2023-12-19 18:12:05 +0530

    Asik

--- src/firebase/Firebase.jsx
+++ src/firebase/Firebase.jsx
@@ -12,16 +12,6 @@ const firebaseConfig = {
     messagingSenderId: "800521501057",
     appId: "1:800521501057:web:b235ee91a179d1661a1080"
 
-
-
-    // original
-
-    // apiKey: "AIzaSyDIn4X6xBB-rZeqCuaO_h8ONcZCtNLjD-w",
-    // authDomain: "alumni-bc761.firebaseapp.com",
-    // projectId: "alumni-bc761",
-    // storageBucket: "alumni-bc761.appspot.com",
-    // messagingSenderId: "922639322609",
-    // appId: "1:922639322609:web:c07243a25a2302d9ff4850"
 };
 
 // Initialize Firebase
